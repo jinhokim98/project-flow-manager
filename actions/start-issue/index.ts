1f494d367ef1e55782602f35d745c527bd1a12f1
김진호 <rlawlsgh1227@gmail.com>
2025-04-22 21:57:51 +0900

    test: info지움

--- actions/start-issue/index.ts
+++ actions/start-issue/index.ts
@@ -11,11 +11,8 @@ async function run() {
   try {
     const token = getInput('github_token');
     const projectOwner = getInput('project_owner');
-    const projectNumber = parseInt(getInput('project_number'), 10);
+    const projectNumber = parseInt(getInput('project_number'));
     const targetColumn = getInput('target_column');
-
-    info(`Project Owner: ${projectOwner}`);
-    info(`Project Number: ${projectNumber}`);
 
     const octokit = getOctokit(token);
 
@@ -27,10 +24,7 @@ async function run() {
       throw new Error('브랜치 이름에서 이슈 번호를 추출할 수 없습니다.');
     }
 
-    const issueNumber = parseInt(issueNumberMatch[1], 10);
-
-    info(`Branch Name: ${branchName}`);
-    info(`Extracted Issue Number: ${issueNumber}`);
+    const issueNumber = parseInt(issueNumberMatch[1]);
 
     const {data: issue} = await octokit.rest.issues.get({
       owner: context.repo.owner,
@@ -38,21 +32,13 @@ async function run() {
       issue_number: issueNumber,
     });
 
-    info(`Issue Node ID: ${issue.node_id}`);
-
     // target column id를 얻어오는 과정
     const projectId = await getProjectId(octokit, projectOwner, projectNumber);
-    info(`Project ID: ${projectId}`);
-
     const {fieldId, options} = await getProjectFieldId(octokit, projectId);
     const statusOptionId = getProjectOptionId(options, targetColumn);
 
-    info(`Field ID: ${fieldId}`);
-    info(`Status Option ID: ${getProjectOptionId(options, targetColumn)}`);
-
     // 이슈 item id를 얻어옴 -> 이미 프로젝트에 등록되어있으면 그 id를 반환하기 때문에 addIssueToProject를 호출해도 무방
     const itemId = await addIssueToProject(octokit, projectId, issue.node_id);
-    info(`Item ID: ${itemId}`);
 
     // 이슈 상태를 특정 상태로 업데이트
     await updateStatusField(octokit, projectId, itemId, fieldId, statusOptionId);
