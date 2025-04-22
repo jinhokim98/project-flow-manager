41d670214f369faed81439ef4bf79dda5d764158
김진호 <rlawlsgh1227@gmail.com>
2025-04-22 14:52:52 +0900

    feat: status를 바꾸는 기능

---
+++ .github/actions/updateStatusField.js
@@ -0,0 +1,33 @@
+const core = require("@actions/core");
+
+export default async function updateStatusField(octokit, projectId, itemId, fieldId, optionId) {
+  const mutation = `
+    mutation($input: UpdateProjectV2ItemFieldValueInput!) {
+      updateProjectV2ItemFieldValue(input: $input) {
+        projectV2Item {
+          id
+        }
+      }
+    }
+  `;
+
+  const variables = {
+    input: {
+      projectId,
+      itemId,
+      fieldId,
+      value: {
+        singleSelectOptionId: optionId,
+      },
+    },
+  };
+
+  const response = await octokit.graphql(mutation, {
+    ...variables,
+    headers: {
+      authorization: `Bearer ${core.getInput("github-token")}`,
+    },
+  });
+
+  return response.updateProjectV2ItemFieldValue.projectV2Item.id;
+}
