3d4e55a442f942ec84a0a0480db66d26d702c6d9
김진호 <rlawlsgh1227@gmail.com>
2025-04-22 20:49:07 +0900

    refactor: project type을 얻어오는 기능 project id를 얻어오는 곳에서 호출

--- actions/utils/getProjectType.ts
+++
@@ -1,6 +0,0 @@
-import {OctokitType} from '../type';
-
-export async function getProjectType(octokit: OctokitType, projectOwner: string) {
-  const res = await octokit.rest.users.getByUsername({username: projectOwner});
-  return res.data.type; // "User" or "Organization"
-}
