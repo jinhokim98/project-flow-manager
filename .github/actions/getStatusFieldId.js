52c7a4a71306dd62e19b46ad5033200624e9bb25
김진호 <rlawlsgh1227@gmail.com>
2025-04-22 14:52:17 +0900

    feat: status field id 가져오는 기능

---
+++ .github/actions/getStatusFieldId.js
@@ -0,0 +1,35 @@
+const core = require("@actions/core");
+
+export default async function getStatusFieldId(octokit, projectId) {
+  const query = `
+    query($projectId: ID!) {
+      node(id: $projectId) {
+        ... on ProjectV2 {
+          fields(first: 50) {
+            nodes {
+              id
+              name
+              dataType
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const response = await octokit.graphql(query, {
+    projectId,
+    headers: {
+      authorization: `Bearer ${core.getInput("github-token")}`,
+    },
+  });
+
+  const fields = response.node.fields.nodes;
+  const statusField = fields.find((field) => field.name === "Status");
+
+  if (!statusField) {
+    throw new Error("'Status' 필드를 찾을 수 없습니다.");
+  }
+
+  return statusField.id;
+}
